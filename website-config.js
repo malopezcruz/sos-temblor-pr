4c48d58aa942d67bd7893a119f84d18a4a7166ae
Marcos A. López Cruz <marquibiris@gmail.com>
2020-01-13 11:00:49 -0400

    Show webiste-config

---
+++ website-config.js
@@ -0,0 +1,65 @@
+module.exports = {
+  pathPrefix: '/',
+  title: '#SOSTemblorPR',
+  titleAlt: 'Lista de recursos',
+  siteUrl: 'https://sostemblorpr.netlify.com/',
+  lang: 'es',
+  locale: 'es',
+  description: 'Lista de campamentos y recursos para la ayuda a damnificadxs',
+  tagline: 'Lista de campamentos y recursos para la ayuda a damnificadxs',
+  companyName: '#SOSTemblorSur',
+  siteLanguage: 'es',
+  sitePublished: '2020',
+  policyDate: '',
+
+  address: {
+    street: '',
+    town: '',
+    state: '',
+    zipcode: '',
+  },
+
+  phoneNumber: '',
+  email: '',
+  whatsAppNumber: '',
+  messangerUser: '',
+
+  socialMedia: {
+    twitterUser: '',
+    facebookUser: '',
+    googleMyBusiness: '',
+    instagramUser: '',
+    youTubeUser: '',
+  },
+
+  googleAnalytics: '',
+  googleSiteVerification: '',
+  msSiteVerification: '',
+  //prettier-ignore
+
+  // JSONLD / Manifest
+  favicon: '',
+  shortName: '',
+  author: '',
+  themeColor: ``,
+  backgroundColor: '',
+
+  // Addional fields for Schema.org
+  siteType: 'Website',
+  siteId: '',
+  siteLogo: '',
+  siteImage: '',
+  latitude: '',
+  longitude: '',
+  hasMap: '',
+  dayOfWeek: '',
+  opens: '',
+  closes: '',
+
+  //More Theme with TailwindCss
+  globalStyle: `text-gray-900 antialiased body`,
+  primaryColor: `222222`,
+
+  //theming
+  navType: ``, // Select from: navRight, navLeft, navCenter
+};
