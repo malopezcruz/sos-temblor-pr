cadf78b7361c7aac2ad098bb26f50372ffcc22d4
Marcos A. López Cruz <marquibiris@gmail.com>
2020-02-12 14:47:44 -0400

    Api key

--- functions/gsheets.js
+++ functions/gsheets.js
@@ -3,6 +3,6 @@ const { GATSBY_PRIVATE_KEY } = process.env;
 exports.handler = async (event, context) => {
   return {
     statusCode: 200,
-    body: JSON.parse(GATSBY_PRIVATE_KEY),
+    body: GATSBY_PRIVATE_KEY,
   };
 };
